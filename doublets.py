25fd7a4f2f15f57c3993134aafaa166c31c9b02b
kevin <610498675@qq.com>
2021-03-19 22:08:38 +0800

    Update doublets.py

--- doublets.py
+++ doublets.py
@@ -17,3 +17,35 @@ f = open(sys.argv[2], "w")
 for d in db[0] :
     f.write(str(d+1) + "\n")
 f.close()
+
+
+
+import numpy as np
+import doubletdetection
+import scanpy as sc
+import matplotlib.pyplot as plt
+
+matrix_path = sys.argv[1]
+adata = sc.read_10x_h5(
+    "pbmc_10k_v3_filtered_feature_bc_matrix.h5", 
+    backup_url="https://cf.10xgenomics.com/samples/cell-exp/3.0.0/pbmc_10k_v3/pbmc_10k_v3_filtered_feature_bc_matrix.h5"
+)
+adata.var_names_make_unique()
+
+# remove "empty" genes
+sc.pp.filter_genes(adata, min_cells=1)
+
+clf = doubletdetection.BoostClassifier(n_iters=50, use_phenograph=False, standard_scaling=True)
+doublets = clf.fit(adata.X).predict(p_thresh=1e-16, voter_thresh=0.8)
+#doublet_score = clf.doublet_score()
+
+db = doublets.nonzero()
+f = open(sys.argv[2], "w")
+for d in db[0] :
+    f.write(str(d+1) + "\n")
+f.close()
+
+
+
+
+
