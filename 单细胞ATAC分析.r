a66a7f1b8c53b7b1b145d0d3def3dffc9d9628cd
kevin <610498675@qq.com>
2021-03-19 15:21:13 +0800

    Update 单细胞ATAC分析.r

--- 单细胞ATAC分析.r
+++ 单细胞ATAC分析.r
@@ -96,9 +96,9 @@ atac_combined <- MergeWithRegions(object.1 = rep1,
 
 atac_combined <- FindTopFeatures(atac_combined, min.cutoff = 0)
 atac_combined <- RunTFIDF(atac_combined)
-atac_combined <- RunSVD(atac_combined, n = 50, reduction.name = 'lsi', reduction.key = 'LSI_')
-#atac_combined <- RunUMAP(atac_combined, dims = 1:30, reduction = 'lsi')
-atac_combined <- RunUMAP(atac_combined, dims = 1:30, reduction = 'lsi',umap.method = "umap-learn")
+atac_combined <- RunSVD(atac_combined, n = 30, reduction.name = 'lsi', reduction.key = 'LSI_')
+atac_combined <- RunUMAP(atac_combined, dims = 1:30, reduction = 'lsi')
+#atac_combined <- RunUMAP(atac_combined, dims = 1:30, reduction = 'lsi',umap.method = "umap-learn")
 
 p1 <- DimPlot(atac_combined, group.by = 'lib', pt.size = 0.1) + ggplot2::ggtitle("Unintegrated")
 ggsave('Unintegrated.pdf',p1)
@@ -111,8 +111,8 @@ atac_combined <- RunHarmony(object = atac_combined,
                             reduction = 'lsi',
                             assay.use = 'peaks',
                             project.dim = FALSE)
-atac_combined <- RunUMAP(atac_combined, dims = 1:30, reduction = 'harmony', umap.method = "umap-learn")
-#atac_combined <- RunUMAP(atac_combined, dims = 1:20, reduction = 'harmony')
+#atac_combined <- RunUMAP(atac_combined, dims = 1:30, reduction = 'harmony', umap.method = "umap-learn")
+atac_combined <- RunUMAP(atac_combined, dims = 1:30, reduction = 'harmony')
 
 p2 <- DimPlot(atac_combined, group.by = 'lib', pt.size = 0.1) + ggplot2::ggtitle("Harmony integration")
 ggsave('refilter.harmony_integration.pdf',p2)
@@ -122,14 +122,14 @@ ggsave('refilter.harmony_integration.pdf',p2)
 #dev.off()
 
 #for clustering only with ATAC, per request of reviewer
-atac_combined <- FindNeighbors(object = atac_combined, reduction = 'lsi', dims = 1:20)
-atac_combined <- FindClusters(object = atac_combined, verbose = FALSE, algorithm = 3, resolution = 0.5)
+atac_combined <- FindNeighbors(object = atac_combined, reduction = 'lsi', dims = 1:30)
+atac_combined <- FindClusters(object = atac_combined, verbose = FALSE, algorithm = 3)
+#atac_combined <- FindClusters(object = atac_combined, verbose = FALSE, algorithm = 3, resolution = 0.5)
+
+p3 <- DimPlot( object=atac_combined, group.by = 'ident', label = TRUE, repel = TRUE) + ggplot2::ggtitle('scATAC-seq self-clustering')
+ggsave("refilter.atac_harmonized_no_RNAseq_integration.clusters.pdf",p3)
+
 saveRDS(atac_combined, "refilter.atac_combined.harmonized_no_RNAseq_integration.RDS")
 write.table(FetchData(atac_combined, "ident"), "refilter.atac_harmonized_no_RNAseq_integration.clusters.csv", sep=",", quote=F)
-pdf("refilter.atac_harmonized_no_RNAseq_integration.clusters.pdf")
-DimPlot(
-object=atac_combined,
- group.by = 'ident',
-  label = TRUE,
-  repel = TRUE) + ggplot2::ggtitle('scATAC-seq self-clustering')
-dev.off()
+
+
